87e2feef8a0ce71b52eeffdb9a1601f4eb00b5e0
drewLM <dphilippidis2@gmail.com>
2024-09-25 14:09:24 +0100

    Add files via upload

---
+++ mycropper.js
@@ -0,0 +1,49 @@
+var width = 1;
+var height = 10;
+
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+    viewMode: 1,
+    dragMode: 'move',
+    aspectRatio: width / height,
+    autoCropArea: 1,
+    restore: false,
+    center: false,
+    cropBoxMovable: false,
+    cropBoxResizable: false,
+    toggleDragModeOnDblclick: false,
+    zoomable: false,
+    background: false,
+    guides: false,
+    center: false,
+});
+
+document.getElementById('cropImageBtn').addEventListener('click', function () {
+    
+    var canvas = cropper.getCroppedCanvas({
+        Width: 256,
+        Height: 256,
+        }).toDataURL('image/png');
+
+    document.getElementById('output').src = canvas;
+    canvas.toBlob(function (blob) {
+      var formData = new FormData();
+
+      $.ajax('croppedImg', {
+        method: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success() {
+          console.log('Upload success');
+          cropperDestory();
+        },
+        error() {
+          console.log('Upload error');
+          refreshCropper();
+        },
+      });
+    }, 'image/png');
+
+
+});
