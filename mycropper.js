5d881be580ab81c8484a4bb210940a8e0de8a398
drewLM <dphilippidis2@gmail.com>
2024-10-28 16:37:25 +0000

    Update mycropper.js

--- mycropper.js
+++ mycropper.js
@@ -1,63 +1,61 @@
+const $image = $('#image');
+
 var width = 1;
 var height = 1;
-const image = document.getElementById('image');
 
-let wallpaper = new Cropper(image, {
-    viewMode: 1,
-    aspectRatio: width/height,
-    dragMode: 'move',
-    autoCropArea: 1,
-    restore: false,
-    center: false,
-    cropBoxMovable: false,
-    cropBoxResizable: false,
-    toggleDragModeOnDblclick: false,
-    zoomable: false,
-    background: false,
-    guides: false,
-    center: false,
+$image.cropper.setDefaults({
+viewMode: 1,
+dragMode: 'move',
+autoCropArea: 1,
+restore: false,
+center: false,
+cropBoxMovable: false,
+cropBoxResizable: false,
+toggleDragModeOnDblclick: false,
+zoomable: false,
+background:false,
+guides: false,
+center: false,
 });
 
 cropperInit();
 
-function cropperInit(){
-  wallpaper = new Cropper(image, {
-    viewMode: 1,
+function cropperInit() {
+    $image.cropper({
     aspectRatio: width/height,
-    dragMode: 'move',
-    autoCropArea: 1,
-    restore: false,
-    center: false,
-    cropBoxMovable: false,
-    cropBoxResizable: false,
-    toggleDragModeOnDblclick: false,
-    zoomable: false,
-    background: false,
-    guides: false,
-    center: false,
-});
+    crop: function(event) {
+            canvas = $image.cropper("getCroppedCanvas", {
+            });        
+   
+    
+        }
+    });
+}
+ 
+function cropperDestory() {
+  $image.cropper("destroy"); 
 }
 
-function cropperDestory() {
-  wallpaper.Cropper.destroy(); 
+function updateCropperImage(url) {
+  $image.attr("src" , url);
+  refreshCropper();
 }
 
 function refreshCropper() {
   cropperDestory();
   cropperInit();
 }
- 
-  window.onmessage = e => {
-    let {data} = e;
-    if(data.toUpdateImageURL) {
-        width = data.widthupdate;
-        height = data.heightupdate;
-        refreshCropper();
 
-        let url = data.updateImageURL;
-        wallpaper.replace(url);
-        
-    }
+
+window.onmessage = e => {
+  let {data} = e;
+  if(data.toUpdateImageURL) {
+      width = data.widthupdate;
+      height = data.heightupdate;
+
+      let url = data.updateImageURL;
+      updateCropperImage(url);
+      
+  
+  }
 }
-
-
