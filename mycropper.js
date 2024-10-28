7ae801897563c67cda367b4fa9d14eedc73cc8b7
drewLM <dphilippidis2@gmail.com>
2024-10-28 15:18:57 +0000

    Update mycropper.js

--- mycropper.js
+++ mycropper.js
@@ -1,30 +1,25 @@
-var width = 1;
-var height = 1;
-
-
 const image = document.getElementById('image');
-const wallpaper = new Cropper(image, {
-    viewMode: 1,
-    dragMode: 'move',
-    aspectRatio: width / height,
-    autoCropArea: 1,
-    restore: false,
-    center: false,
-    cropBoxMovable: false,
-    cropBoxResizable: false,
-    toggleDragModeOnDblclick: false,
-    zoomable: false,
-    background: false,
-    guides: false,
-    center: false,
-});
 
- 
-  window.onmessage = e => {
+window.onmessage = e => {
     let {data} = e;
     if(data.toUpdateImageURL) {
-        width = data.widthupdate;
-        height = data.heightupdate;
+        let width = data.widthupdate;
+        let height = data.heightupdate;
+        const wallpaper = new Cropper(image, {
+          viewMode: 1,
+          dragMode: 'move',
+          aspectRatio: width / height,
+          autoCropArea: 1,
+          restore: false,
+          center: false,
+          cropBoxMovable: false,
+          cropBoxResizable: false,
+          toggleDragModeOnDblclick: false,
+          zoomable: false,
+          background: false,
+          guides: false,
+          center: false,
+      });
 
         let url = data.updateImageURL;
         wallpaper.replace(url);
